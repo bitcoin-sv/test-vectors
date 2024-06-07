1889d3405f72d121efcd3dd709e701255e11d7c7
Oli Oskarsson <oli@oskarsson.nl>
2024-06-07 14:01:34 +0200

    Leave in the comments from SV Node

--- data/import-from-svnode/index.js
+++ data/import-from-svnode/index.js
@@ -2,10 +2,19 @@ const fs = require('fs');
 const createCsvWriter = require('csv-writer').createArrayCsvWriter;
 
 function jsonToCsv(sourceFile, targetFile, headers) {
-    const jsonData = JSON.parse(fs.readFileSync(sourceFile, 'utf-8'));
     const csvWriter = createCsvWriter({
         path: targetFile,
         header: headers
+    });
+    let jsonData = JSON.parse(fs.readFileSync(sourceFile, 'utf-8'));
+    // Modify rows with only one cell, these are comments and will be marked
+    // when parsing this data, make sure to use a library that support comments
+    // e.g. https://csv.js.org/parse/options/comment/
+    jsonData = jsonData.map(row => {
+        if (row.length === 1) {
+            row[0] = `# ${row[0]}`;
+        }
+        return row;
     });
 
     csvWriter.writeRecords(jsonData)
@@ -16,6 +25,6 @@ jsonToCsv(
     '../../../bitcoin-sv/src/test/data/script_tests.json',
     '../script_vectors.csv',
     // TODO: improve this, straight copy from the bitcoin-sv JSON file
-    ['[[wit..., amount]?', 'scriptSig', 'scriptPubKey', 'flags', 'expected_scripterror', 'comments']
+    ['[[wit..., amount]?', 'scriptSig', 'flags', 'expected_scripterror', 'comments']
 );
 // TODO: import the others from sv-node
